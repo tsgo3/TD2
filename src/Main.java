4fc10d8cd5cb84056cff3c316c6dec85984fd3ca
tsgo3 <39001182@parisnanterre.fr>
2022-10-24 15:48:53 +0200

    exo 1 question 1 et 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Somme<Integer> ajoutentier = (a, b) -> a+b;
+        Somme<Double> ajoutdouble = (a, b) -> a + b;
+        Somme<Long> ajoutlong = (a, b) -> a+b;
+        Somme<String> ajoutstring = (a, b) -> a+b;
+
+        Somme<Integer> ajoutentier2 = Main::somme;
+
+        System.out.println(ajoutentier.somme(9,8));
+        System.out.println(ajoutdouble.somme(123.5,2.45));
+        System.out.println(ajoutlong.somme(12l,35436l));
+        System.out.println(ajoutstring.somme("alors","non"));
+        System.out.println(ajoutentier2.somme(45,36));
+
+        ToString<String> convert = (a) -> ;
     }
-}
+    public static int somme(int a, int b){
+        return a + b;
+    }
+    }
---
+++ src/ToString.java
@@ -0,0 +1,3 @@
+public interface ToString <T>{
+    String tostring(T a);
+}
